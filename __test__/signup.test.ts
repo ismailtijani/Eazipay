d9c752696065fd9d4e23dd5db7887addc5108698
Ismail Tijani <ismailtijani10@yahoo.com>
2023-10-07 22:37:29 +0100

    Signup Test

--- __test__/signup.test.ts
+++ __test__/signup.test.ts
@@ -2,16 +2,31 @@ import { app } from "../src/config/app";
 import request from "supertest";
 
 describe("Signup", () => {
-  it('should "Create a new user account"', async () => {
+  it("Should sign up a new user", async () => {
     const signup = `
-      query {
-        home
-      }
+    mutation {
+      signup(
+        signupInputs: {
+          firstName: "John",
+          lastName: "Doe",
+          email: "john.doe@example.com",
+          password: "password123",
+          phoneNumber: "1234567890"
+        }
+      )
+    }
+
     `;
 
-    const response = await request(app).post("/graphql").send({ signup }).expect(200);
+    try {
+      const response = await request(app).post("/graphql").send({ query: signup }).expect(200);
 
-    const responseData = response.body.data;
-    expect(responseData.signup).toEqual("Account created successfuly!");
+      const responseData = response.body.data;
+      console.log(response.body.errors);
+      // Check if the signup was successful
+      expect(responseData.signup).toEqual("Account created successfuly!");
+    } catch (error) {
+      throw error;
+    }
   });
 });
