12439f7b884f951d8f28377b0be85d9e565a8abf
Ismail Tijani <ismailtijani10@yahoo.com>
2023-10-26 21:33:29 +0100

    Removing unused variables

--- src/library/constants.ts
+++ src/library/constants.ts
@@ -3,6 +3,5 @@ export const ACCOUNT_SUCCESS_SUBJECT = "Account Creation Success";
 export const PASSWORD_RESET_HELP = "Password Reset Help";
 export const AUTH_CODE_SUBJECT = "Two-factor Authentication Code";
 export const ACCESS_TOKEN = "Access Token";
-export const TOP_PRODUCT = "Top Products";
 export const RESET_PASSWORD = "resetPassword?";
 export const ADMIN = "Admin?";
