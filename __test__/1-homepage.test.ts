1218cbcab3984061a5279c9d78b7482bfafaec8c
Ismail Tijani <ismailtijani10@yahoo.com>
2023-11-26 19:30:01 +0100

    C

--- __test__/1-homepage.test.ts
+++ __test__/1-homepage.test.ts
@@ -1,20 +1,20 @@
 import { app } from "../src/config/app";
 import request from "supertest";
 
-describe("Homepage", () => {
-  it('should return "Welcome to Eazipay Application" for the home query', async () => {
-    const query = `
-      query {
-        home
-      }
-    `;
+// describe("Homepage", () => {
+//   it('should return "Welcome to Eazipay Application" for the home query', async () => {
+//     const query = `
+//       query {
+//         home
+//       }
+//     `;
 
-    const response = await request(app).post("/graphql").send({ query }).expect(200);
+//     const response = await request(app).post("/graphql").send({ query }).expect(200);
 
-    const responseData = response.body.data;
-    expect(responseData.home).toEqual("Welcome to Eazipay Application");
-  });
-});
+//     const responseData = response.body.data;
+//     expect(responseData.home).toEqual("Welcome to Eazipay Application");
+//   });
+// });
 
 // import { server } from "../src/config/app";
 // it("Homepage", async () => {
